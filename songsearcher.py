596219f5f2fcf77dfc7d46c51e2a14f289fa1f57
samjhopkins9 <samjhopkins9@gmail.com>
2023-01-31 16:34:21 -0500

    Added space to code

--- songsearcher.py
+++ songsearcher.py
@@ -2,10 +2,14 @@
 import csv
 import webbrowser as web
 
+
 # Opens "songs_to_search.csv"
 with open("songs_to_search.csv") as file:
+
     # Loops through all lines in file
     for line in csv.reader(file):
+    
     # Opens a web browser tab containing a link to an Amazon Search using the first cell in the row as the title of the song, and the second cell as the artist.
         web.open("https://www.amazon.com/s?k={a1}+{t1}&i=digital-music&crid=J8V6U287ZTGH&sprefix={a1}+{t1}%2Cdigital-music%2C83&ref=nb_sb_noss_1".format(t1=line[0], a1=line[1]))
+        
     # Assumes that the only text contained within the .csv file is the titles of the songs in the first column and the artists in the second.
--- songsorter.py
+++ songsorter.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
+
 # Playlists array is declared empty, to contain names of playlists to add a song file to
 playlists = []
 
 # Function takes a song filename as an input, prints its name, and prompts the user to select which playlist folders they would like to add it to, and how many. It then adds the names of the folders to the playliss array, which later is read by the Bash script and used to copy the songs into organized directories.
 def sort_song(song):
+
     print(song)
     print("Playlists")
     print("1. Hits/Throwbacks")
@@ -17,36 +19,53 @@ def sort_song(song):
     print("8. Other")
     print("9. Originals")
     print("10. REDWAVE.")
+    
     # Asks user how many playlists to add so that it can prompt a correct number of times
     pln = input("How many playlists would you like to add this song to?: ")
     range1 = int(pln)
+    
     # All songs are automatically added to 'All' playlist
     playlists.append("All")
+    
     # Loops through number of playlists inputted by user
     for _ in range(range1):
         i = input("Enter playlist: ")
+        
         if i == "1":
             playlists.append("Hits_Throwbacks")
+            
         elif i == "2":
             playlists.append("Rap")
+        
         elif i == "3":
             playlists.append("Deep_ChillHouse")
+        
         elif i == "4":
             playlists.append("Tech_BassHouse")
+        
         elif i == "5":
             playlists.append("Trance_ProgHouse")
+        
         elif i == "6":
             playlists.append("Trap_Dubstep")
+        
         elif i == "7":
             playlists.append("Drum&Bass")
+        
         elif i == "8":
             playlists.append("Other")
+        
         elif i == "9":
             playlists.append("Original")
+            
         elif i == "10":
             playlists.append("REDWAVE.")
+            
         # If a value that doesn't correspond to anything is inputted, the function is called recursively until they do
         else:
             print("Invalid choice. Try again.")
             sort_song(song);
+        
+    # end of for loop
 
+# end of sort_song function
